52e72ceed80e7d6ee5be11d8db6fe00eee544beb
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-08-06 15:11:19 -0300

    refactor: refatora a classe database e inclue arquivo env de exemplo

--- backend/config/database_config.py
+++ backend/config/database_config.py
@@ -1,8 +1,16 @@
-from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import sessionmaker
 
-DATABASE_URL = "postgresql://'':''@localhost/livraria"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
