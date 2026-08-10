1d2dce1ee9bea5cfd2e394516621d157dbc4fa40
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-08-10 18:02:34 -0300

    refactor: refatora entidades do models

--- backend/models/book.py
+++ backend/models/book.py
@@ -5,10 +5,38 @@ from backend.config.database_config import Base
 
 class Book(Base):
     __tablename___= 'books'
-    id = Column(Integer, primary_key=true, index=true)
-    nome = Column(String(100), nullable=false)
-    descricao = Column(String(200), nullable=false)
-    valor = Column(Integer, nullable=false)
-    url_imagem = Column(String, nullable=false)
+    
+    id = Column(
+        "id",
+        Integer, 
+        primary_key=True, 
+        autoincrement=True)
+    
+    nome = Column(
+        "nome",
+        String(100), 
+        nullable=False)
+    
+    descricao = Column(
+        "descricao",
+        String(200), 
+        nullable=False)
+    
+    valor = Column(
+        "preco",
+        Integer, 
+        nullable=False)
+    
+    url_imagem = Column(
+        "url",
+        String, 
+        nullable=False)
+
+    
+    def __init__(self, nome, descricao, valor, url_imagem):
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = valor
+        self.url_imagem = url_imagem
 
     
