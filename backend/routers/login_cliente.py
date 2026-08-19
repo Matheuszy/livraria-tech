5e25b7c16d905a869329e1a99133bfc32ee7674b
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-08-19 17:56:42 -0300

    refactor: incrementa cliente com endereco e ajusta lógica de schemas

--- backend/routers/login_cliente.py
+++ backend/routers/login_cliente.py
@@ -11,11 +11,23 @@ login_router = APIRouter(prefix="/login", tags=["login"])
 @login_router.post("/cliente-cadastro")
 async def cliente_cadastro(cliente_schema : ClienteSchema, session = Depends(get_session)):
     cliente = session.query(models.Cliente).filter(models.Cliente.email == cliente_schema.email).first()
-    if not cliente:
-        raise HTTPException(status_code=400, detail="E-mail de usuário não cadastrado")
+    if cliente:
+        raise HTTPException(status_code=400, detail="E-mail de usuário já cadastrado")
     else:
+        novo_endereco = Endereco(rua=cliente_schema.endereco.rua,
+                                 numero=cliente_schema.endereco.numero,
+                                 bairro=cliente_schema.endereco.bairro,
+                                 cidade=cliente_schema.endereco.cidade,
+                                 estado=cliente_schema.endereco.estado,
+                                 cep=cliente_schema.endereco.cep)
         cript = bcrypt.hash(cliente_schema.password)
-        novo_cliente = Cliente(cliente_schema.nome, cliente_schema.email, cript, cliente_schema.telefone)
+        novo_cliente = Cliente(cliente_schema.nome,
+                               cliente_schema.age,
+                               cliente_schema.email,
+                               cript,
+                               cliente_schema.telefone,
+                                novo_endereco
+                               )
         session.add(novo_cliente)
         session.commit()
         return {"message": f"Cliente cadastrado com sucesso {cliente_schema.nome} + {cliente_schema.email}"}
