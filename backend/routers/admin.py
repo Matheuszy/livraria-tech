2061f3efdbae54bd8875c9fb05b395762787d552
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-08-24 15:24:46 -0300

    fix: realizei a refatoracao e renomeacao de rotas de cadastro de admin

--- backend/routers/admin.py
+++ backend/routers/admin.py
@@ -5,9 +5,9 @@ from backend.config.dependencies import get_session
 from backend.config.crypt_config import bcrypt
 from backend.schemas.admin_schema import AdminSchema
 
-auth_router = APIRouter(prefix="/auth", tags=["auth"])
+admin_router = APIRouter(prefix="/auth", tags=["auth"])
 
-@auth_router.post("/criar-conta")
+@admin_router.post("/criar-conta")
 async def criar_conta(admin_schema: AdminSchema, session:Session = Depends(get_session)):
 
     admin = session.query(Admin).filter(Admin.email == admin_schema.email).first()
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from backend.routers.auth import auth_router
+from backend.routers.admin import admin_router
 from backend.routers.orders import order_router
 from backend.routers.login_cliente import login_router
 
 
 app = FastAPI()
-app.include_router(auth_router)
+app.include_router(admin_router)
 app.include_router(login_router)
 app.include_router(order_router)
