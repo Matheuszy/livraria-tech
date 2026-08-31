4bfc3d67e33da609b008f46a244b30fdc248297e
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-08-30 21:01:20 -0300

    feat: implementando a autenticacao para o login do admin

--- backend/routers/admin.py
+++ backend/routers/admin.py
@@ -3,7 +3,7 @@ from backend.models.admin import Admin
 from sqlalchemy.orm import sessionmaker, Session
 from backend.config.dependencies import get_session
 from backend.config.crypt_config import bcrypt
-from backend.schemas.admin_schema import AdminSchema
+from backend.schemas.admin_schema import AdminSchema, LoginAdmin
 
 admin_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -20,3 +20,12 @@ async def criar_conta(admin_schema: AdminSchema, session:Session = Depends(get_s
         session.commit()
         return {"message": f"Conta criada com sucesso {novo_admin.email}"}
 
+@login_admin.post("/admin/logar")
+async def login_admin(admin_login: LoginAdmin, session: Session = Depends(get_session)):
+    admin = session.query(Admin).filter(Admin.email == admin_login.email).first()
+
+    if not admin or token_jwt(admin.id):
+        raise HTTPException(status_code=401, detail="E-mail ou senha incorretos")
+    else:
+        access_token = token_jwt(admin.id)
+        return {"acess_token": access_token, "token_type": "Bearer"}
